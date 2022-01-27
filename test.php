f535cd6f85fd2d6e80279956501b9f92bcd6c40d
firman <firmanraiwan@gmail.com>
2022-01-27 16:45:37 +0700

    clean master project

--- test.php
+++
@@ -1,9 +0,0 @@
-<?php
-date_default_timezone_set('Asia/Jakarta');
-$awal  = date_create('2022-01-07 15:00:00');
-$akhir = date_create(); // waktu sekarang
-$diff  = date_diff($awal,$akhir);
-
-echo $diff->h . ' jam, ';
-echo $diff->i . ' menit, ';
-echo $diff->s . ' detik, ';
